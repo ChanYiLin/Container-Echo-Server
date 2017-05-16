f0ce97bbdda6204280c50d12ec83715dbfccd924
ChanYiLin <j5111261112@gmail.com>
2017-05-16 15:58:13 +0800

    mnt fixed

--- containerClient/rootfs/mnt_echo_client.c
+++ containerClient/rootfs/mnt_echo_client.c
@@ -35,62 +35,11 @@ int main(int argc, char *argv[])
 		printf("inotify_add_watch\n");
 		return 1;
 	}
-	int ret;
-	FILE *fp_c = fopen("client_message", "w");
-	char input[4096], ch;
-	while((ch = getchar()) != '\n'){
-		ret = fputc(ch, fp_c);
-		if(ret < 0){
-			printf("put char error!");
-			perror(strerror(errno));
-		}
-		printf("receive char: %c\n",ch);
-	}
-	fputc('\n', fp_c);
-	fclose(fp_c);
+
+
 
 	while(1){
 
-		// 1. send the message through writing to the client_message file
-		// 2. client_message file will be read  and  deleted by bridge
-		// 3. bridge will then send the message to server and receive the msg from server
-		// 4. bridge will create the bridge_message file and write the message into it.
-		// 5. after detecting the closing of the bridge_message file then open, read and delete the file 
-		
-
-
-
-
-		numRead = read(inotifyFd, buf, BUF_LEN);
-		if (numRead <= 0) {
-			perror(strerror(errno));
-			printf("read() from inotify fd returned %d!", numRead);
-			return 1;
-		}
-
-		for (p = buf; p < buf + numRead; ) {
-			event = (struct inotify_event *) p;
-			if((event->mask|IN_CLOSE_WRITE) && !strcmp(event->name, "bridge_message")){
-
-				FILE *fp_b = fopen("bridge_message", "r");
-				char ch;
-
-				printf("Recv:");
-				while((ch = fgetc(fp_b)) != '\n')
-					putchar(ch);
-				printf("\n");
-				fclose(fp_b);
-				system("rm -f bridge_message");
-
-
-				break;
-
-			}
-
-			p += sizeof(struct inotify_event) + event->len;
-		}
-
-		
 		FILE *fp_c = fopen("client_message", "w");
 		char input[4096], ch;
 		while((ch = getchar()) != '\n'){
@@ -99,6 +48,50 @@ int main(int argc, char *argv[])
 		}
 		fputc('\n', fp_c);
 		fclose(fp_c);
+
+		// 1. send the message through writing to the client_message file
+		// 2. client_message file will be read  and  deleted by bridge
+		// 3. bridge will then send the message to server and receive the msg from server
+		// 4. bridge will create the bridge_message file and write the message into it.
+		// 5. after detecting the closing of the bridge_message file then open, read and delete the file 
+		int flag = 0;
+
+
+		while(1){
+			
+			numRead = read(inotifyFd, buf, BUF_LEN);
+			if (numRead <= 0) {
+				perror(strerror(errno));
+				printf("read() from inotify fd returned %d!", numRead);
+				return 1;
+			}
+
+			for (p = buf; p < buf + numRead; ) {
+				event = (struct inotify_event *) p;
+				if((event->mask|IN_CLOSE_WRITE) && !strcmp(event->name, "bridge_message")){
+
+					FILE *fp_b = fopen("bridge_message", "r");
+					char ch;
+
+					printf("Recv:");
+					while((ch = fgetc(fp_b)) != '\n')
+						putchar(ch);
+					printf("\n");
+					fclose(fp_b);
+					system("rm -f bridge_message");
+
+					flag=1;
+					break;
+
+				}
+
+				p += sizeof(struct inotify_event) + event->len;
+			}
+			if(flag==1)
+				break;
+		}
+
+
 
 	}
 }
